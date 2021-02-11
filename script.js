6558e6e854699d6d6a5d0c4cb2047506b9661e66
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-02-11 15:35:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ var game = {
       game.over += 9;
       
       do {
-        game.over -= Math.ceil((game.over + 10) / 2 - 0.1);
+        game.over -= Math.ceil((game.over + 10) / 2);
         game.ord += 10;
       } while (game.over + 10 >= 20 && game.ord % 100 != 0);
       
