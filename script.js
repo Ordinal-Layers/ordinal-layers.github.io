fc875c443e3a005ab04fae677f674f7c632c3367
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-02-11 15:16:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -31,55 +31,59 @@ var game = {
   },
   header: document.getElementById("header"),
   writeOrd: function() {
-    var notation = "";
-    var ordinal = game.ord;
-    
-    while (ordinal > 0) {
-      function log10(arg) {
-        return Math.log(arg) / Math.LN10;
-      }
-      
-      var power = Math.floor(log10(ordinal));
-      
-      if (notation == "") {
-        if (power == 0) {
-          notation = ordinal + game.over;
-        } else {
-          if (power == 1) {
-            if (Math.floor(ordinal / 10) == 1) {
-              notation = "&omega;";
-            } else {
-              notation = "&omega;" + Math.floor(ordinal / 10);
-            }
+    if (game.ord == 0) {
+      game.header.innerHTML = "H<sub>0</sub>(10)";
+    } else {
+      var notation = "";
+      var ordinal = game.ord;
+
+      while (ordinal > 0) {
+        function log10(arg) {
+          return Math.log(arg) / Math.LN10;
+        }
+
+        var power = Math.floor(log10(ordinal));
+
+        if (notation == "") {
+          if (power == 0) {
+            notation = ordinal + game.over;
           } else {
-            if (Math.floor(ordinal / 10 ** power) == 1) {
-              notation = "&omega;<sup>" + power + "</sup>";
+            if (power == 1) {
+              if (Math.floor(ordinal / 10) == 1) {
+                notation = "&omega;";
+              } else {
+                notation = "&omega;" + Math.floor(ordinal / 10);
+              }
             } else {
-              notation = "&omega;<sup>" + power + "</sup>" + Math.floor(ordinal / 10 ** power);
+              if (Math.floor(ordinal / 10 ** power) == 1) {
+                notation = "&omega;<sup>" + power + "</sup>";
+              } else {
+                notation = "&omega;<sup>" + power + "</sup>" + Math.floor(ordinal / 10 ** power);
+              }
+            }
+          }
+        } else {
+          if (power == 0) {
+            notation = notation + "+" + (ordinal + game.over);
+          } else {
+            if (power == 1) {
+              if (Math.floor(ordinal / 10) == 1) {
+                notation = notation + "+&omega;";
+              } else {
+                notation = notation + "+&omega;" + Math.floor(ordinal / 10);
+              }
+            } else {
+              if (Math.floor(ordinal / 10 ** power) == 1) {
+                notation = notation + "+&omega;<sup>" + power + "</sup>";
+              }
+
+              notation = notation + "+&omega;<sup>" + power + "</sup>" + Math.floor(ordinal / 10 ** power);
             }
           }
         }
-      } else {
-        if (power == 0) {
-          notation = notation + "+" + (ordinal + game.over);
-        } else {
-          if (power == 1) {
-            if (Math.floor(ordinal / 10) == 1) {
-              notation = notation + "+&omega;";
-            } else {
-              notation = notation + "+&omega;" + Math.floor(ordinal / 10);
-            }
-          } else {
-            if (Math.floor(ordinal / 10 ** power) == 1) {
-              notation = notation + "+&omega;<sup>" + power + "</sup>";
-            }
-            
-            notation = notation + "+&omega;<sup>" + power + "</sup>" + Math.floor(ordinal / 10 ** power);
-          }
-        }
+
+        ordinal -= 10 ** power * Math.floor(ordinal / 10 ** power);
       }
-      
-      ordinal -= 10 ** power * Math.floor(ordinal / 10 ** power);
     }
     
     game.header.innerHTML = "H<sub>" + notation + "</sub>(10)"
