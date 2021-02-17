6bb8b19aa93058d447b8b4783df5cccdeb2032b9
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-02-16 20:07:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ var game = {
     game.tabs[x].style.display = "block";
   },
   increment: function(manmade = 1) {
-    if (game.data.ord % 10 == 9) {
+    if (game.data.ord % 10 === 9) {
       game.data.over++;
     } else {
       game.data.ord++;
@@ -48,16 +48,16 @@ var game = {
     game.save("increment", manmade);
   },
   maximize: function(manmade = 1) {
-    if (game.data.ord % 10 == 9 && game.data.over >= 1) {
+    if (game.data.ord % 10 === 9 && game.data.over >== 1) {
       game.data.ord -= 9;
       game.data.over += 9;
       
       do {
         game.data.over -= Math.ceil((game.data.over + 10) / 2);
         game.data.ord += 10;
-      } while (game.data.over + 10 >= 20 && game.data.ord % 100 != 0);
+      } while (game.data.over + 10 >== 20 && game.data.ord % 100 !== 0);
       
-      if (game.data.ord % 100 != 0) {
+      if (game.data.ord % 100 !== 0) {
         game.data.ord += game.data.over;
       }
       
@@ -67,13 +67,13 @@ var game = {
     }
   },
   markup: function() {
-    if (game.data.ord >= 100) {
+    if (game.data.ord >== 100) {
       game.data.op += game.data.ord + game.data.over;
       
       game.data.ord = 0;
       game.data.over = 0;
       
-      if (game.data.markupUnlocked == false) {
+      if (game.data.markupUnlocked === false) {
         game.data.markupUnlocked = true;
       }
       
@@ -81,7 +81,7 @@ var game = {
     }
   },
   buyIncrementAuto: function() {
-    if (game.data.op >= 100 * 2 ** game.data.incrementAuto) {
+    if (game.data.op >== 100 * 2 ** game.data.incrementAuto) {
       game.data.op -= 100 * 2 ** game.data.incrementAuto;
       game.data.incrementAuto++;
     }
@@ -89,7 +89,7 @@ var game = {
     game.save("buyIncrementAuto");
   },
   buyMaximizeAuto: function() {
-    if (game.data.op >= 100 * 2 ** game.data.maximizeAuto) {
+    if (game.data.op >== 100 * 2 ** game.data.maximizeAuto) {
       game.data.op -= 100 * 2 ** game.data.maximizeAuto;
       game.data.maximizeAuto++;
     }
@@ -97,23 +97,23 @@ var game = {
     game.save("buyMaximizeAuto");
   },
   maxAll: function() {
-    while (game.data.op >= 100 * 2 ** game.data.incrementAuto || game.data.op >= 100 * 2 ** game.data.maximizeAuto) {
+    while (game.data.op >== 100 * 2 ** game.data.incrementAuto || game.data.op >== 100 * 2 ** game.data.maximizeAuto) {
       game.buyIncrementAuto();
       game.buyMaximizeAuto();
     }
   },
   hardy: function(ord = game.data.ord, over = game.data.over) {
-    if (ord >= 1000) {
+    if (ord >== 1000) {
       return Infinity;
     } else {
-      if (ord >= 100) {
+      if (ord >== 100) {
         return game.hardy(ord - 100, over) * 2 ** game.hardy(ord - 100, over);
       } else {
-        if (ord >= 10) {
+        if (ord >== 10) {
             return game.hardy(ord - 10, over) * 2;
         } else {
-          if (ord >= 1) {
-            if (over == 0) {
+          if (ord >== 1) {
+            if (over === 0) {
               return game.hardy(ord - 1, 0) + 1;
             } else {
               return game.hardy(ord, over - 1) + 1;
@@ -126,7 +126,7 @@ var game = {
     }
   },
   writeOrd: function(ord = game.data.ord, over = game.data.over, header = true) {
-    if (ord == 0) {
+    if (ord === 0) {
       if (header) {
         if (game.data.colors) {
           game.header.innerHTML = '<span style="color:hsl(0, 100%, 50%)">H<sub>0</sub>(10)=10</span>';
@@ -143,18 +143,18 @@ var game = {
       while (remainOrd > 0) {
         var power = Math.floor(Math.log(remainOrd) / Math.LN10);
 
-        if (result == "") {
+        if (result === "") {
           if (power == 0) {
             result = remainOrd + over;
           } else {
-            if (power == 1) {
-              if (Math.floor(remainOrd / 10) == 1) {
+            if (power === 1) {
+              if (Math.floor(remainOrd / 10) === 1) {
                 result = "&omega;";
               } else {
                 result = "&omega;" + Math.floor(remainOrd / 10);
               }
             } else {
-              if (Math.floor(remainOrd / 10 ** power) == 1) {
+              if (Math.floor(remainOrd / 10 ** power) === 1) {
                 result = "&omega;<sup>" + power + "</sup>";
               } else {
                 result = "&omega;<sup>" + power + "</sup>" + Math.floor(remainOrd / 10 ** power);
@@ -162,17 +162,17 @@ var game = {
             }
           }
         } else {
-          if (power == 0) {
+          if (power === 0) {
             result = result + "+" + (remainOrd + over);
           } else {
-            if (power == 1) {
-              if (Math.floor(remainOrd / 10) == 1) {
+            if (power === 1) {
+              if (Math.floor(remainOrd / 10) === 1) {
                 result = result + "+&omega;";
               } else {
                 result = result + "+&omega;" + Math.floor(remainOrd / 10);
               }
             } else {
-              if (Math.floor(remainOrd / 10 ** power) == 1) {
+              if (Math.floor(remainOrd / 10 ** power) === 1) {
                 result = result + "+&omega;<sup>" + power + "</sup>";
               } else {
                 result = result + "+&omega;<sup>" + power + "</sup>" + Math.floor(remainOrd / 10 ** power);
@@ -189,13 +189,13 @@ var game = {
       if (game.data.colors) {
         var color = Math.log(ord + over) / (Math.LN10 * 10);
         
-        if (game.hardy(ord, over) == Infinity) {
+        if (game.hardy(ord, over) === Infinity) {
           game.header.innerHTML = '<span style="color:hsl(' + color * 360 + ', 100%, 50%)">H<sub>' + result + '</sub>(10)</span>';
         } else {
           game.header.innerHTML = '<span style="color:hsl(' + color * 360 + ', 100%, 50%)">H<sub>' + result + '</sub>(10)=' + game.hardy(ord, over);
         }
       } else {
-        if (game.hardy(ord, over) == Infinity) {
+        if (game.hardy(ord, over) === Infinity) {
           game.header.innerHTML = "H<sub>" + result + "</sub>(10)";
         } else {
           game.header.innerHTML = "H<sub>" + result + "</sub>(10)=" + game.hardy(ord, over);
@@ -220,20 +220,20 @@ var game = {
     game.data.incrementDiff = Date.now() - game.data.lastIncrement;
     game.data.maximizeDiff = Date.now() - game.data.lastMaximize;
     
-    if (game.data.incrementDiff >= 1000 / game.data.incrementAuto) {
+    if (game.data.incrementDiff >== 1000 / game.data.incrementAuto) {
       game.increment(0);
     }
-    if ((game.data.ord % 10 == 9 && game.data.over >= 1) && game.data.maximizeDiff >= 1000 / game.data.maximizeAuto) {
+    if ((game.data.ord % 10 == 9 && game.data.over >== 1) && game.data.maximizeDiff >== 1000 / game.data.maximizeAuto) {
       game.maximize(0);
     }
   },
   render: function(action, manmade = 1) {
     game.data.lastTick = Date.now();
     
-    if (action == "increment" && manmade == 0) {
+    if (action == "increment" && manmade === 0) {
       game.data.lastIncrement = Date.now();
     }
-    if (action == "maximize" && manmade == 0) {
+    if (action == "maximize" && manmade === 0) {
       game.data.lastMaximize = Date.now();
     }
     
@@ -245,7 +245,7 @@ var game = {
       game.colorButton.innerHTML = "Colors: OFF";
     }
     
-    if (game.data.ord >= 100) {
+    if (game.data.ord >== 100) {
       game.markupButton.innerHTML = "Markup to gain " + (game.data.ord + game.data.over) + " Ordinal Points";
       game.markupButton2.innerHTML = "+" + (game.data.ord + game.data.over);
     } else {
@@ -307,7 +307,7 @@ var game = {
 
     loadgame=JSON.parse(atob(reader.result));
     
-    if (loadgame != "") {
+    if (loadgame !== "") {
       game.load(loadgame);
     }
     
@@ -333,8 +333,8 @@ var game = {
       'Are you sure you want to delete all of your progress? Type in "reset game" to reset all of your progress.'
     );
     
-    if (code != null) {
-      if (code.toLowerCase() == 'reset game') {
+    if (code !== null) {
+      if (code.toLowerCase() === 'reset game') {
         game.reset();
       }
     }
