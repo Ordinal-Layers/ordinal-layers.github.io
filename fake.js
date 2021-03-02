24719d6d60b828abacce88ba01498c164022f897
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-03-01 22:44:00 -0800

    Update fake.js

--- fake.js
+++ fake.js
@@ -13,4 +13,16 @@ if (realSites.includes(sPath)) {
 if (real) {
   document.getElementById("game").style.display = "block";
   document.getElementById("fake").style.display = "none";
+  
+  var extend1 = document.createElement("script");
+  extend1.src = "extend/jquery.js";
+  document.body.appendChild(extend1);
+  
+  var extend2 = document.createElement("script");
+  extend2.src = "extend/notify.js";
+  document.body.appendChild(extend2);
+  
+  var script = document.createElement("script");
+  script.src = "script.js";
+  document.body.appendChild(script);
 }
