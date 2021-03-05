6a0ab84ccc6f75ee5b246abe77ee17de6b0ff212
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-03-04 19:52:07 -0800

    Update fake.js

--- fake.js
+++ fake.js
@@ -14,15 +14,33 @@ if (real) {
   document.getElementById("game").style.display = "block";
   document.getElementById("fake").style.display = "none";
   
-  var extend1 = document.createElement("script");
-  extend1.src = "extend/jquery.js";
-  document.body.appendChild(extend1);
+  function script1() {
+    var extend1 = document.createElement("script");
+    extend1.src = "extend/jquery.js";
+    document.body.appendChild(extend1);
+  }
   
-  var extend2 = document.createElement("script");
-  extend2.src = "extend/notify.js";
-  document.body.appendChild(extend2);
+  function script2() {
+    var extend2 = document.createElement("script");
+    extend2.src = "extend/notify.js";
+    document.body.appendChild(extend2);
+  }
   
-  var script = document.createElement("script");
-  script.src = "script.js";
-  document.body.appendChild(script);
+  function script3() {
+    var script = document.createElement("script");
+    script.src = "script.js";
+    document.body.appendChild(script);
+  }
+  
+  function script4() {
+    script2();
+    setTimeout(script3, 50);
+  }
+  
+  function script() {
+    script1();
+    setTimeout(script4, 50);
+  }
+  
+  setTimeout(script, 50);
 }
