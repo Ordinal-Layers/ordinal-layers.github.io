9d5ae644e632f062a136b5484f80d9779e526e79
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-03-09 13:54:16 -0800

    Update load.js

--- load.js
+++ load.js
@@ -1,7 +1,8 @@
 var real = false;
 var sPath = location.origin;
+var inPublicTesting;
 
-if (sPath === "https://ordinal-layers.github.io" || sPath === "https://ordinal-layers.github.io/index.html" || sPath === "https://ordinal-layers.github.io/index") {
+if (sPath === "https://ordinal-layers.github.io") {
   real = true;
 }
 
@@ -9,30 +10,34 @@ if (real) {
   document.getElementById("fake").style.display = "none";
   document.getElementById("loadingScreen").style.display = "block";
   
-  function script1() {
+  inPublicTesting = () => {
+    return location.href.split("/")[3] === "public.html" || location.href.split("/")[3] === "public";
+  };
+  
+  var script1 = () => {
     var extend1 = document.createElement("script");
     extend1.src = "extend/jquery.js";
     document.body.appendChild(extend1);
-  }
+  };
   
-  function script2() {
+  var script2 = () => {
     var extend2 = document.createElement("script");
     extend2.src = "extend/notify.js";
     document.body.appendChild(extend2);
   }
   
-  function script3() {
+  var script3 = () => {
     var script = document.createElement("script");
     script.src = "script.js";
     document.body.appendChild(script);
   }
   
-  function script4() {
+  var script4 = () => {
     script2();
     setTimeout(script3, 200);
   }
   
-  function script() {
+  var script = () => {
     script1();
     setTimeout(script4, 200);
   }
