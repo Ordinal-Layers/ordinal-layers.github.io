3d694a838dbd9d2720cb2ab3d351cfa1eb0823c8
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-03-09 20:28:22 -0800

    Update load.js

--- load.js
+++ load.js
@@ -10,34 +10,34 @@ if (real) {
   document.getElementById("fake").style.display = "none";
   document.getElementById("loadingScreen").style.display = "block";
   
-  inPublicTesting = () => {
+  inPublicTesting = function() {
     return location.href.split("/")[3] === "public.html" || location.href.split("/")[3] === "public";
   };
   
-  var script1 = () => {
+  function script1() {
     var extend1 = document.createElement("script");
     extend1.src = "extend/jquery.js";
     document.body.appendChild(extend1);
-  };
+  }
   
-  var script2 = () => {
+  function script2() {
     var extend2 = document.createElement("script");
     extend2.src = "extend/notify.js";
     document.body.appendChild(extend2);
   }
   
-  var script3 = () => {
+  function script3() {
     var script = document.createElement("script");
     script.src = "script.js";
     document.body.appendChild(script);
   }
   
-  var script4 = () => {
+  function script4() {
     script2();
     setTimeout(script3, 200);
   }
   
-  var script = () => {
+  function script() {
     script1();
     setTimeout(script4, 200);
   }
