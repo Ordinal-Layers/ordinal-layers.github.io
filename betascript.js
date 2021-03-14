427ac554a87f5448364098eecde2e7dc0c65f3f3
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-03-13 17:47:03 -0800

    Update betascript.js

--- betascript.js
+++ betascript.js
@@ -299,16 +299,6 @@ var game = {
     [66, 66, 21, Infinity],
     [55, 78, 66, Infinity]
   ],
-  chalGoals: [
-    [1.000e30, 1.000e200, 2.000e230, Infinity],
-    [2.000e230, game.V(9) + 1.000e230, game.V(18) + 1.000e230, Infinity],
-    [1.000e200, 1.000e215, 1.000e230, Infinity],
-    [1.000e30, 1.000e100, 1.000e120, Infinity],
-    [1.000e100, 1.000e135, 1.000e210, Infinity],
-    [1.000e30, 1.000e42, 1.000e108, Infinity],
-    [1.000e12, 1.000e15, 1.000e21, Infinity],
-    [1.000e10, 2.000e10, 8.000e10, Infinity]
-  ],
   achievementsEarned: () => {
     var achieveCount = 0;
     for (var y = 0; y < game.data.achievements.length; y++) {
@@ -329,6 +319,16 @@ var game = {
     }
     return boost;
   },
+  chalGoals: [
+    [1.000e30, 1.000e200, 2.000e230, Infinity],
+    [2.000e230, game.V(9) + 1.000e230, game.V(18) + 1.000e230, Infinity],
+    [1.000e200, 1.000e215, 1.000e230, Infinity],
+    [1.000e30, 1.000e100, 1.000e120, Infinity],
+    [1.000e100, 1.000e135, 1.000e210, Infinity],
+    [1.000e30, 1.000e42, 1.000e108, Infinity],
+    [1.000e12, 1.000e15, 1.000e21, Infinity],
+    [1.000e10, 2.000e10, 8.000e10, Infinity]
+  ],
   totalChalComp: () => {
     var comp = 0;
     
