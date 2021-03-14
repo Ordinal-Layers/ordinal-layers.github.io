8701cf6ed8604114df8ff499305e4d816bc509d2
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-03-14 15:22:51 -0700

    Update betascript.js

--- betascript.js
+++ betascript.js
@@ -1642,6 +1642,7 @@ var game = {
       newGame.boosterUnlocked = false;
       newGame.factorBoosts = 0;
       newGame.dynamicFactor = 1;
+      newGame.highestLevel = 0;
       newGame.achievements = [
         [false, false, false, false, false, false, false, false, false, false]
       ];
